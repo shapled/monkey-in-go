dba19645d67e070013878cb05e46f884548a0be5
shapled <shapled@shapled.com>
2023-07-05 20:34:47 +0800

    add compiler

---
+++ compiler/compiler.go
@@ -0,0 +1,88 @@
+package compiler
+
+import (
+	"monkey/ast"
+	"monkey/code"
+	"monkey/object"
+)
+
+// Bytecode 是编译的结果
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
+
+type Compiler struct {
+	instructions code.Instructions // 指令纸袋
+	constants    []object.Object   // 常量列表
+}
+
+func NewCompiler() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+// 编译的过程中会修改填充 instructions 和 constants
+func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+	}
+
+	return nil
+}
+
+// 将得到的对象压入常量池，返回索引 id
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+// 写入编译得到的新的 instruction
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+	ins := code.Make(op, operands...)
+	pos := c.addInstruction(ins)
+	return pos
+}
+
+// 返回插入的新 instructions 的 offset
+func (c *Compiler) addInstruction(ins []byte) int {
+	posNewInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return posNewInstruction
+}
